d302087a1055108f2671e1460c673156b32a46ff
Administrator <admin@mcsog.tk>
2024-07-31 00:33:09 +0800

    移除plugins目录

--- plugins/test_plugin/__init__.py
+++
@@ -1,2 +0,0 @@
-from .info import plugin_name,plugin_id,plugin_version,plugin_author,plugin_desc
-from .test_plugin import Plugin
--- plugins/test_plugin/info.py
+++
@@ -1,6 +0,0 @@
-plugin_name="test plugin"
-plugin_id="tk.mcsog.test_plugin"
-plugin_version="1.0.0"
-plugin_author="f00001111"
-plugin_desc="测试"
-#plugin_info=(plugin_version,plugin_name,plugin_id,'register','enable','disable','unregister')
